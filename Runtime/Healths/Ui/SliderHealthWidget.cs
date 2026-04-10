0f73e197603362db1fefd67834399bdd948db169
Kira Dinan <kiradinan.dev@gmail.com>
2026-04-10 16:25:39 -0300

    Update SliderHealthWidget.cs

--- Runtime/Healths/Ui/SliderHealthWidget.cs
+++ Runtime/Healths/Ui/SliderHealthWidget.cs
@@ -1,17 +1,8 @@
 // Copyright Falling Snow Interactive 2025
 
-using TMPro;
-using UnityEngine;
-using UnityEngine.UI;
-
 namespace Fsi.Gameplay.Healths.Ui
 {
 	public class SliderHealthWidget : HealthWidget
 	{
-		
-		
-		
-		
-		
 	}
 }
