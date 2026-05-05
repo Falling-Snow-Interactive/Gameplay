d5783ee65cf7baeb5f0539e263aa124c6b94a6b4
Kira Dinan <kiradinan.dev@gmail.com>
2026-05-05 15:44:25 -0300

    Update SampleIdleState.cs

--- Sample/Scripts/Gameplay/Players/StateMachines/States/SampleIdleState.cs
+++ Sample/Scripts/Gameplay/Players/StateMachines/States/SampleIdleState.cs
@@ -1,36 +1,10 @@
-using DG.Tweening;
 using Fsi.StateMachines;
-using UnityEngine;
 
 namespace Fsi.Gameplay.Sample.Gameplay.Players.StateMachines.States
 {
 	public class SampleIdleState : MonoState
 	{
-		[SerializeField]
-		private float minTime;
-
-		private bool canExit;
-
-		public override void OnEnter()
-		{
-			base.OnEnter();
-			canExit = false;
-		    
-			Sequence s = DOTween.Sequence();
-			s.AppendInterval(minTime)
-			 .AppendCallback(() => canExit = true);
-
-			s.Play();
-		}
-
-		public override void OnExit()
-		{
-			base.OnExit();
-			canExit = false;
-		}
-
 		public override bool CanTransitionIn() => true;
-
-		public override bool CanTransitionOut() => canExit;
+		public override bool CanTransitionOut() => true;
 	}
 }
